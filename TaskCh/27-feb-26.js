58085dcc4605e2a6d658b9e646770bd678102acd
Rabbi Hossain <rabbihossainlhp@gmail.com>
2026-02-27 19:40:35 +0600

    api caching

--- TaskCh/27-feb-26.js
+++ TaskCh/27-feb-26.js
@@ -2,7 +2,7 @@
 function removeDuplicates(arr) {
     let storeResult = {}
     for(let i=0; i<arr.length; i++){
-        if(!storeResult.hasOwnProperty(arr[i].toString())){
+        if(!storeResult.hasOwnProperty(arr[i])){
             storeResult[arr[i]] = arr[i];
         }
     }
@@ -11,3 +11,34 @@ function removeDuplicates(arr) {
 
 
 console.log(removeDuplicates([4,3,5,5,2,1,3]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Simple data caching during api calling concept...
+const cache = {};
+
+async function getProfile() {
+  if (cache["profile"]) {
+    console.log("Using cached data");
+    return cache["profile"];
+  }
+
+  console.log("Calling API...");
+  const response = await fetch("/api/profile");
+  const data = await response.json();
+
+  cache["profile"] = data;
+
+  return data;
+}
