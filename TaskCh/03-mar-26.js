e5d2563c0a63687639bd72a70e26b9a7651c6f7d
Rabbi Hossain <rabbihossainlhp@gmail.com>
2026-03-03 22:08:13 +0600

    added 3-march-26

---
+++ TaskCh/03-mar-26.js
@@ -0,0 +1,78 @@
+
+//again remove duplicate using "map"...
+const removeDuplicates = (arr)=>{
+    const map = new Map();
+    for(let i=0; i<arr.length; i++){
+        if(!map.has(arr[i])){
+            map.set(arr[i],true);
+        }
+    }
+    return Array.from(map.keys())
+    
+}
+
+
+
+console.log(removeDuplicates([3,2,2,4,43,3]))
+
+
+
+
+
+
+
+
+
+///Another core Interview patter. with "map" internal logic...
+function countFrequency(arr) {
+    const map = new Map();
+    for(let i=0; i<arr.length; i++){
+        if(!map.has(arr[i])){
+            map.set(arr[i],1);
+        }
+        else{
+            map.set(arr[i], map.get(arr[i])+1);
+        }
+    }
+
+    return map;
+}
+
+
+
+console.log(countFrequency(["a","d","a"]))
+
+
+
+
+
+
+
+
+
+
+
+
+//Now this challenge comes from real job scenario.....
+
+function getDashInfo(arr){
+    const map = new Map();
+    for(let i=0; i<arr.length; i++){
+        for(let key in arr[i]){
+            map.has(arr[i][key]) ? map.set(arr[i][key], map.get(arr[i][key])+1) : map.set(arr[i][key],1);
+        }
+    }
+
+    return map;
+}
+
+
+console.log(getDashInfo(
+    [
+  { status: "completed" },
+  { status: "pending" },
+  { status: "completed" },
+  { status: "failed" },
+  { status: "pending" }
+]
+))
