65feba8b932d2680f74fd0e23800cb8620941b0f
Rabbi Hossain <rabbihossainlhp@gmail.com>
2026-03-15 05:52:56 +0600

    BST insert method

--- Tree/Binary_search_tree.js
+++ Tree/Binary_search_tree.js
@@ -13,12 +13,48 @@ class Node {
 class BST {
     constructor() {
         this.root = null;
+    }
 
+
+    insert(value){
+        const newNode = new Node(value);
+
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+
+        let temp = this.root;
+
+        while(true){
+            if(newNode.value === temp.value){
+                return undefined
+            }
+
+            if(newNode.value < temp.value){
+                if(temp.left === null){
+                    temp.left = newNode;
+                    return this;
+                }else{
+                    temp = temp.left;
+                }
+                
+            }else{
+                if(temp.right === null){
+                    temp.right = newNode;
+                    return this;
+                }
+                temp = temp.right;
+            }
+        }
+        
     }
 }
 
 
 const tree = new BST();
 
-
+tree.insert(2);
+tree.insert(2);
+tree.insert(5);
 console.log(tree)
